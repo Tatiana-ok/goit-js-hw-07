f5ce35c43291fcbaa5e9ff53e95228bc02536f84
Tetiana Pomazunovska <my.mail.tetiana@gmail.com>
2020-10-25 22:34:46 +0200

    Re-submit

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,14 @@
 const categories = document.querySelectorAll('.item');
 console.log(`В списке ${categories.length} категории.`);
 
-for(let i = 0; i < categories.length; i += 1) {
-    const ulFirst = categories[i];
+// for(let i = 0; i < categories.length; i += 1) {
+//     const ulFirst = categories[i];
+//     console.log(`Категория: ${ulFirst.firstElementChild.textContent}`);
+//     console.log(`Количество элементов: ${ulFirst.lastElementChild.children.length}`);
+// }
+
+categories.forEach(function (categor) {
+    const ulFirst = categor;
     console.log(`Категория: ${ulFirst.firstElementChild.textContent}`);
     console.log(`Количество элементов: ${ulFirst.lastElementChild.children.length}`);
-}
+});
